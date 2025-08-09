2dcfb39cb96330ae35b673c1c75bb7ab8cb8dcf2
Samuel Campelo <samuelcampelo25@gmail.com>
2025-08-08 22:50:46 -0300

    Verifica se o usuário inputou algo

--- static/js/script.js
+++ static/js/script.js
@@ -6,6 +6,7 @@ function add_spring() {
     const springs_div = document.querySelector("#springs")
     const fieldset = document.createElement("fieldset")
     const fieldset_legend = document.createElement("legend")
+    const warning = document.createElement("p")
     
     fieldset_legend.innerText = `Mola ${counter}`
     
@@ -17,7 +18,7 @@ function add_spring() {
         const label = document.createElement("label")
         const br = document.createElement("br")
         
-        label.innerText = `Medição ${c}`
+        label.innerText = `Medição ${c} (em metro)`
         measurement.classList.add("measurement_input")
         
         fieldset.append(measurement)
@@ -31,6 +32,9 @@ function add_spring() {
     submit_button.type = "submit"
     submit_button.value = "Enviar"
     submit_button.classList.add("submit_button")
+    submit_button.onclick = function() {
+        check_filled_out(submit_button, warning)
+    }
     
     delete_button.type = "button"
     delete_button.value = "Deletar"
@@ -38,14 +42,47 @@ function add_spring() {
     delete_button.onclick = function() {
         delete_spring(delete_button)
     } 
-        
     
     fieldset.appendChild(submit_button)
     fieldset.appendChild(delete_button)
+    fieldset.appendChild(warning)
 }
+
 
 function delete_spring(delete_button) {
     const fieldset = delete_button.closest("fieldset")
-
+    
     fieldset.remove()
+}
+
+
+function check_filled_out(submit_button, warning) {
+    const fieldset = submit_button.closest("fieldset")
+    const inputs = fieldset.querySelectorAll(".measurement_input")
+
+    let measurements = []
+    let filled = true
+
+    inputs.forEach(input => {
+        if (input.value.trim() === "") {
+            filled = false
+        } else if (isNaN(parseFloat(input.value.trim()))) {
+            filled = false
+        } else {
+            measurements.push(parseFloat(input.value.trim()))
+        }
+    })
+
+    if (!filled) {
+        warning.innerText = "Por favor, preencha todos os campos com números antes de enviar"
+        warning.style.color = "red"
+    } else {
+        let total = 0
+        for (let measurement in measurements){
+            total += measurements[measurement]
+        }
+
+        warning.innerText = `Dados enviados com sucesso, comprimento médio de ${(total/6).toFixed(2)}m`
+        warning.style.color = "green"
+    }
 }
